21449a7f3944af969ab58a3e30469fba1b9fb064
andwis177 <andwis177@gmail.com>
2024-06-21 16:23:03 +0200

    initial commit

---
+++ backend/travel-with-anna/src/test/java/com/andwis/travel_with_anna/TravelWithAnnaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andwis.travel_with_anna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelWithAnnaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
